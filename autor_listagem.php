d0b6751a23223b29281491ea8983c9497ab0a2ba
nardo <129193276+nardomaciel@users.noreply.github.com>
2024-03-18 17:30:23 -0300

    novo autor

---
+++ autor_listagem.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>NOVO AUTOR</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+  <div class="container">
+    <h2>AUTORES</h2>
+    <div class="table-responsive">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>João</td>
+            
+            <td>
+              <button class="btn btn-primary">Editar</button>
+              <button class="btn btn-danger">Excluir</button>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Maria</td>
+        
+            <td>
+              <button class="btn btn-primary">Editar</button>
+              <button class="btn btn-danger">Excluir</button>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Carlos</td>
+        
+            <td>
+              <button class="btn btn-primary">Editar</button>
+              <button class="btn btn-danger">Excluir</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</body>
+</html>
